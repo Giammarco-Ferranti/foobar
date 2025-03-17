57246b5e0380b172819d2811b2cc6de1aaf7a641
Giammi <ferrantigiammarco@gmail.com>
2025-03-17 11:48:20 +0100

    first commit

---
+++ foobar.js
@@ -0,0 +1,15 @@
+const fooBar = () => {
+  for (let i = 0; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FooBar");
+    } else if (i % 3 === 0) {
+      console.log("Foo");
+    } else if (i % 5 === 0) {
+      console.log("Bar");
+    } else {
+      console.log(i);
+    }
+  }
+};
+
+fooBar();
